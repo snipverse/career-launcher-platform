49feef1d02525bef7e412b96b122f14809a58a34
snipverse <sachinpal9324@gmail.com>
2025-07-08 12:06:30 +0530

    Day 3 of week 2 completed with adding live search , refresh , score

--- backend/app.py
+++ backend/app.py
@@ -20,6 +20,22 @@ def save_data(data):
     with open(DATA_FILE, 'w') as f:
         json.dump(data, f, indent=4)
 
+def calculate_score(resume):
+    score = 0
+    fields = ['name', 'skills', 'email', 'education', 'experience', 'projects', 'certifications']
+    per_field_score = 100 / len(fields)
+
+    for field in fields:
+        if resume.get(field):
+            if isinstance(resume[field], list):
+                if len(resume[field]) > 0:
+                    score += per_field_score
+            elif isinstance(resume[field], str):
+               if resume[field].strip():
+                   score += per_field_score
+
+    return round(score)
+
 @app.route('/')
 def home():
     return "Welcome to Resume API"
@@ -34,7 +50,15 @@ def submit_resume():
 
 @app.route('/resume', methods=['GET'])
 def get_resumes():
-    return jsonify(load_data())
+    try:
+        resumes = load_data()  # ✅ FIXED: was loading wrong file before
+        for r in resumes:
+            r['score'] = calculate_score(r)
+
+        return jsonify(resumes)
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
